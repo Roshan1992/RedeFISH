474da3a3300b05c47f60ee8436a6fc4f7f07fe18
Yunshan Zhong <327922729@qq.com>
2025-03-31 11:26:49 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(name='RedeFISH',
-      author="Zhong Yunsan",
+      author="Zhong Yunshan",
       author_email="327922729@qq.com",
-      description="Automatic Cell Alignment in Ultra-large Spatial and Single-cell Transcriptomics Data",
-      version='v1.0.0',
+      description="Cell segmentation and gene imputation for imaging-based spatial transcriptomics",
+      version='V1.0.0',
       packages=find_packages(),
       )
